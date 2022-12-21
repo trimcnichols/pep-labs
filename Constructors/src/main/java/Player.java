3fe5462ae828590cad8758deca4d6273b772c5a4
trimcnichols <trimcnichols@protonmail.com>
2022-12-21 16:39:14 +0000

    Saved progress on Instantiation 2022-12-21 16:39:14 +0000

--- Constructors/src/main/java/Player.java
+++ Constructors/src/main/java/Player.java
@@ -15,8 +15,9 @@
  **/
 public class Player {
     public String playerName;
+     
     public int level;
-
+    
     /**
      * this.playerName should be set to playerName.
      *
@@ -31,7 +32,9 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-
+        this.playerName = playerName;
+        this.level = 1;
+       
     }
 
     /**
@@ -48,7 +51,11 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
-
+        
+        Player newPlayer = new Player("Sam",5);
+        
+       
+       
     }
 
 }
