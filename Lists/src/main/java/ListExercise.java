046a9880864ecd5725de75590042ec0c4309492e
trimcnichols <trimcnichols@protonmail.com>
2022-12-24 15:15:55 +0000

    Saved progress on Sets 2022-12-24 15:15:55 +0000

--- Lists/src/main/java/ListExercise.java
+++ Lists/src/main/java/ListExercise.java
@@ -30,7 +30,7 @@ public class ListExercise {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        List<Integer> list = new ArrayList<Integer> ();
+       
 
         return list;
     }
