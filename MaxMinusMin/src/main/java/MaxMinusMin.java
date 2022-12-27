708299ed360290f1c322bc52308005563d7513a9
trimcnichols <trimcnichols@protonmail.com>
2022-12-27 20:11:25 +0000

    Saved progress on GetMax 2022-12-27 20:11:25 +0000

--- MaxMinusMin/src/main/java/MaxMinusMin.java
+++ MaxMinusMin/src/main/java/MaxMinusMin.java
@@ -1,5 +1,5 @@
 
-import java.util.List;
+import java.util.*;
 
 public class MaxMinusMin {
     /**
@@ -10,6 +10,12 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
+        List<Integer> nums = new ArrayList<Integer>();
+        int max =0;
+        for (int i = 0 ; i < nums.length; i++ ){
+            
+        }
+
         return 0;
     }
 }
