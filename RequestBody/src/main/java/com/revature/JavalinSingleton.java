f90354e32592868ffe08142287e9f544afcb89ff
trimcnichols <trimcnichols@protonmail.com>
2023-01-04 14:38:30 +0000

    Saved progress on IndexOf 2023-01-04 14:38:30 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 
 /**
@@ -16,11 +18,25 @@ public class JavalinSingleton {
          * 
          * Note: Please refer to the "RequestBody.MD" file for more assistance if needed.
          */
-        app.post("/songName", ctx -> {
-           
-               ctx.result(ctx.body());
+        app.post("/song", ctx -> {
+                      
+             //retrieve the json string from the request body
+                 String jsonString = ctx.body();
+
+             //utilize jackson to convert the json string to a user object
+                 ObjectMapper om = new ObjectMapper();
+                 Song song = om.readValue(jsonString, Song.class);
+
+            //we need to let the request know we will send back json in the body
+                 ctx.contentType("application/json"); 
+
+                 //return the json string in the response body
+                 String artistName = om.writeValueAsString(song);
+                 ctx.result(artistName);
+                  });
+            
               
-        });
+       
 
          /**
          * problem2: retrieve the song object from the request body...
@@ -29,8 +45,25 @@ public class JavalinSingleton {
          * 
          * Note: Please refer to the "RequestBody.MD" file for more assistance if needed.
          */
-        app.post("/problem2", ctx -> {
-            //implement logic here
+        app.post("/song", ctx -> {
+             //retrieve the json string from the request body
+    String jsonString = ctx.body();
+
+    //utilize jackson to convert the json string to a user object
+    ObjectMapper om = new ObjectMapper();
+    Song song = om.readValue(jsonString, Song.class);
+
+    //we need to let the request know we will send back json in the body
+    ctx.contentType("application/json"); 
+
+    //change the artist name
+    song.artistName("Beatles");
+    
+    //utilize jackson convert back the user object to a json string
+    String artistName = om.writeValueAsString(song);
+
+    //return the json string in the response body
+    ctx.result(artistName);
      });
 
 
