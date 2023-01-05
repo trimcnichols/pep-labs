8761947e92a0abc238f0151de36518ec2b5d760d
trimcnichols <trimcnichols@protonmail.com>
2023-01-05 18:34:11 +0000

    Saved progress on OuterJoin 2023-01-05 18:34:11 +0000

---
+++ OuterJoin/problem1.sql
@@ -0,0 +1,1 @@
+SELECT * FROM class LEFT OUTER JOIN student on class.class_title = student.class_title;
---
+++ OuterJoin/problem2.sql
@@ -0,0 +1,1 @@
+SELECT * FROM class RIGHT OUTER JOIN textbook on class.class_title = textbook.class_title;
