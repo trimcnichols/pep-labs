e290a7d39315dd7980258adf6ae1dfd6e11077ec
trimcnichols <trimcnichols@protonmail.com>
2023-01-09 16:09:29 +0000

    Saved progress on AdvancedBooleans 2023-01-09 16:09:29 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
+
 
 public class CombineArrays {
     /**
@@ -7,8 +10,17 @@ public class CombineArrays {
      * @param arr2 an array of ints.
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
-    public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+    public ArrayList<Integer> combine(int[] arr1, int[] arr2){
+        //int[] arr = new int[arr1.length+arr2.length];
+        ArrayList<Integer> arr3 = new ArrayList<>();
+        
+        for (int i = 0; i < arr1.length; i++) {
+            arr3.add(arr1[i]);
+        }
+        for (int j = 0; j < arr2.length; j++){
+            arr3.add(arr2[j]);
+        }
+       
+        return arr3;
     }
 }
