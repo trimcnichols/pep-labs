bbf84bd18d5422a774443db541ad380ab17dfe51
trimcnichols <trimcnichols@protonmail.com>
2023-01-11 20:45:55 +0000

    Saved progress on OtherStringMethods 2023-01-11 20:45:55 +0000

--- OtherStringMethods/src/main/java/OtherStringMethods.java
+++ OtherStringMethods/src/main/java/OtherStringMethods.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Official String documentation: https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
  */
@@ -11,7 +13,8 @@ public class OtherStringMethods {
      * @return
      */
     public String partOfString(String s, int start, int end){
-        return "";
+        String str = s.substring(start,end);
+        return str;
     }
 
     /**
@@ -26,7 +29,17 @@ public class OtherStringMethods {
      * and 0 if s1 is equivalent to s2.
      */
     public int compareLexigraphically(String s1, String s2){
+        if (s1.compareTo(s2) >0 ){
+            return 1;
+            
+        }
+        else if (s1.compareTo(s2) < 0){
+            return -1;
+
+        }
+        else {
         return 0;
+        }
     }
 
     /**
@@ -39,6 +52,9 @@ public class OtherStringMethods {
      * @return an array of Strings that represent s1 split by splitAround.
      */
     public String[] splitStringIntoMultipleStrings(String s1, String splitAround){
-        return null;
+       
+       String[] arr = s1.split(splitAround);
+
+        return arr;
     }
 }
