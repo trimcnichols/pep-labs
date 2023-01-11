94081bf9272884a5a60567feb4ce2739b3235390
trimcnichols <trimcnichols@protonmail.com>
2023-01-11 14:01:25 +0000

    Saved progress on CombineArrays 2023-01-11 14:01:25 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -11,17 +11,17 @@ public class CombineArrays {
      * @param arr2 an array of ints.
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
-    public ArrayList<Integer> combine(int[] arr1, int[] arr2){
+    public int[] combine(int[] arr1, int[] arr2){
         int[] arr = new int[arr1.length+arr2.length];
-        ArrayList<Integer> arr3 = new ArrayList<>();
+       // ArrayList<Integer> arr3 = new ArrayList<>();
         
         for (int i = 0; i < arr1.length; i++) {
-            arr3.add(arr1[i]);
+            arr[i] = arr1[i];
         }
         for (int j = 0; j < arr2.length; j++){
-            arr3.add(arr2[j]);
+            arr[arr1.length+j] =  arr2[j];
         }
      
-        return arr3;
+        return arr;
     }
 }
