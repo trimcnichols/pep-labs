6c5cb629eb0260b0667717a9c57c311b94632595
trimcnichols <trimcnichols@protonmail.com>
2023-01-12 20:22:56 +0000

    Saved progress on WordCount 2023-01-12 20:22:56 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,7 +5,16 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
+       
+        int wordCount = 1;
+
+
+        for (int i = 0 ; i <= in.length()-1; i++){
+            if(in.charAt(i) == ' ' && in.charAt(i+1) != ' '){
+                wordCount++;
+            }
+        }
         
-        return 0;
+        return wordCount;
     }
 }
