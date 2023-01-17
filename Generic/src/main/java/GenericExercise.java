c759ca596bd36843af84779bd521b128b0d7357b
trimcnichols <trimcnichols@protonmail.com>
2023-01-17 19:36:48 +0000

    Saved progress on Generic 2023-01-17 19:36:48 +0000

--- Generic/src/main/java/GenericExercise.java
+++ Generic/src/main/java/GenericExercise.java
@@ -21,14 +21,15 @@ public class GenericExercise<T> {
      * @param in an Object that will be of type T at runtime.
      */
     public void loadGenericItem(T in){
-
+       
+        this.data = in;
     }
     /**
      * @return T data. The type of Data will again be set when the GenericExercise object is created (at runtime).
      */
     public T returnGenericItem(){
-
-         <T> in = new <T>(data);
-        return data;
+       
+       
+        return this.data;
     }
 }
