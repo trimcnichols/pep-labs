dc3f648e4dc7e10c4150478d59fff7ea42af4d2c
trimcnichols <trimcnichols@protonmail.com>
2023-01-19 21:09:11 +0000

    Saved progress on InsertPosition 2023-01-19 21:09:11 +0000

--- InsertPosition/src/main/java/InsertPosition.java
+++ InsertPosition/src/main/java/InsertPosition.java
@@ -15,6 +15,14 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        int i;
+        for ( i = 0; i < nums.length; i++){
+            if (nums[i]> target){
+                target = nums[i];
+                break;
+                
+            }
+        }
+       return i; 
     }
 }
