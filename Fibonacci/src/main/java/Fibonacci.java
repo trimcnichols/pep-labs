babbfdf0f3d1466af2b14089085d37ee58d1be5e
trimcnichols <trimcnichols@protonmail.com>
2023-01-20 21:38:50 +0000

    Saved progress on Fibonacci 2023-01-20 21:38:50 +0000

--- Fibonacci/src/main/java/Fibonacci.java
+++ Fibonacci/src/main/java/Fibonacci.java
@@ -18,20 +18,19 @@ public class Fibonacci {
         
     int n0 = 0;
     int n1 = 1;
-    int num = 0;
-   
+    int i = 1;
+   int num = 0;
         if (n==0 || n ==1){
             return n;
         }
         else {
-        for (int i = 2; i < n ; i++){
+      while(i<n ){
             num= n0 + n1;
             n0 = n1;
             n1= num;
+            i++;
         }
-         
-        }
-    
+    }
         return num;
     }
 }
